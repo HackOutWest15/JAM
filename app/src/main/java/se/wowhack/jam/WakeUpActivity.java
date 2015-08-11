7dcf7f0707ae3bff7a565f13bd995aed90387555
Jonathan Nilsfors <jonathan.nilsfors@gmail.com>
2015-08-12 00:21:09 +0200

    WORKS

--- app/src/main/java/se/wowhack/jam/WakeUpActivity.java
+++ app/src/main/java/se/wowhack/jam/WakeUpActivity.java
@@ -71,7 +71,7 @@ public class WakeUpActivity extends Activity implements PlayerNotificationCallba
             public void onInitialized(Player player) {
                 player.addConnectionStateCallback(WakeUpActivity.this);
                 player.addPlayerNotificationCallback(WakeUpActivity.this);
-                player.play("spotify:track:2TpxZ7JUBn3uw46aR7qd6V");
+                player.play("spotify:track:5XsMz0YfEaHZE0MTb1aujs");
             }
 
             @Override
@@ -97,49 +97,6 @@ public class WakeUpActivity extends Activity implements PlayerNotificationCallba
         PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK, "My Tag");
 
         wl.acquire();
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
-    }
-
-
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
-        }
-    };
-
-    Handler mHideHandler = new Handler();
-    Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            mSystemUiHider.hide();
-        }
-    };
-
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
     }
 
     @Override
