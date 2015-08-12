ca455c9639dd05c99cabe8861b7f561c9996dc43
Jonathan Nilsfors <jonathan.nilsfors@gmail.com>
2015-08-12 12:55:01 +0200

    Send the playlist id on broadcast

--- app/src/main/java/se/wowhack/jam/AlarmReceiver.java
+++ app/src/main/java/se/wowhack/jam/AlarmReceiver.java
@@ -13,11 +13,15 @@ public class AlarmReceiver extends WakefulBroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().substring(0,11).equals("alarmAction")) {
-            Log.d("Playlist from alarmReceiver", intent.getStringExtra("Playlist"));
+            //Log.d("Playlist from alarmReceiver", intent.getStringExtra("Playlist"));
 
             Log.d("#################", "Larm fired event");
             // For our recurring task, we'll just display a message
             Intent wakeUpIntent = new Intent(context, WakeUpService.class);
+            if(intent.getStringExtra("Playlist") != null) {
+                String pl = intent.getStringExtra("Playlist");
+                wakeUpIntent.putExtra("Playlist", pl);
+            }
             startWakefulService(context,wakeUpIntent);
         }
     }
